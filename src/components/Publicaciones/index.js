a041b735cfad0cf94de68ad6cf38c62392c96cb3
Claudia Aparicio <clau.ach07@gmail.com>
2020-01-25 02:19:20 -0400

    conectar reducer y traer info de url

---
+++ src/components/Publicaciones/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect} from 'react-redux'
+import reducers from '../../reducers';
+import * as usuariosActions from '../../actions/usuariosActions'
+
+class Publicaciones extends Component {
+    componentDidMount() {
+        if (!this.props.usuarios.length) {
+
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+                <h1>
+                    Publicaciones de 
+                </h1>
+                { this.props.match.params.key}
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (reducers) => {
+    return reducers.usuariosReducer
+}
+
+export default connect(mapStateToProps)(Publicaciones);
